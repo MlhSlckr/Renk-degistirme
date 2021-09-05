0c01972745e4052e3473afb1ea11497f55b331e6
MlhSlckr <selcukermelih@gmail.com>
2021-09-05 13:44:50 +0300

    a

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('.color-change');
 const text = document.querySelector('.color-text');
+const body = document.querySelector('body');
 const codesArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
 let x;
 
@@ -14,7 +15,8 @@ function changeColor() {
 button.addEventListener('click', () => {
   const colorCode = changeColor();
   text.innerHTML = colorCode;
-  document.body.style.backgroundColor = colorCode;
+  body.style.backgroundColor = colorCode;
   button.style.color = colorCode;
   text.style.color = colorCode;
 })
+
